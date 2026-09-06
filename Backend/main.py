19a5ec84ff7f15806f00a85d7bc94fd4a8a5c635
Chinmayananda Dhupad <chinmaydhupad@gmail.com>
2026-09-06 19:57:10 +0530

    Build backend registration and database setup

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from passlib.context import CryptContext
+from database import engine, Base, SessionLocal
+from models.user import UserModel
+from schemas.user import UserCreate, UserResponse
 
 app = FastAPI(title="Artisan AI Backend")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+Base.metadata.create_all(bind = engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def root():
     return {
         "message": "Artisan AI Backend is running"
     }
+
+@app.post("/register",  response_model = UserResponse)
+def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    new_user= UserModel(
+        username = user.username, 
+        email=user.email,
+        password=pwd_context.hash(user.password)
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
